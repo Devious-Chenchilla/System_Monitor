f9c31a7ada7bcf859ae5bd054198fcad24038687
Uvsq <lounes.kheris98@gmail.com>
2021-02-17 17:43:19 +0100

    First commit

---
+++ build/CMakeFiles/smon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smon.dir/src/main.c.o"
+  "CMakeFiles/smon.dir/src/system.c.o"
+  "smon"
+  "smon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/smon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include "system.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/select.h>
-#include <termios.h>
-#include <signal.h>
